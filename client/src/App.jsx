4d40b1efb2b9db49f9f40b9237de5751fd4859fa
Chetan <51225903+ChetanKawarkhe@users.noreply.github.com>
2025-12-20 13:36:05 +0530

    Authentication and authorisation logic implemented for routing. Next steps Login and Registration forms.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,32 +13,52 @@ import ShoppingAccount from "./pages/shopping-view/account"
 import ShoppingCheckout from "./pages/shopping-view/checkout"
 import ShoppingListing from "./pages/shopping-view/listing"
 import ShoppingHome from "./pages/shopping-view/home"
+import CheckAuth from "./components/common/check-auth"
+import UnauthPage from "./indesx"
 
 function App() {
+
+  const isAuthenticated = true;
+  const user = null;
 
   return (
     <div className="flex flex-col overflow-hidden bg-white">
 
       <Routes>
-        <Route path="/auth" element={<AuthLayout/>}>
+        <Route path="/auth" element={
+          <CheckAuth isAuthenticated = {isAuthenticated} user = {user}>
+            <AuthLayout/>
+          </CheckAuth>
+        }>
           <Route path="login" element={<AuthLogin/>}></Route>
           <Route path="register" element={<AuthRegister/>}></Route>
         </Route>
-        <Route path="admin" element={<AdminLayout/>}>
+        <Route path="admin" element={
+          <CheckAuth isAuthenticated = {isAuthenticated} user = {user}>
+            <AdminLayout/>
+          </CheckAuth>
+        }>
           <Route path="products" element={<AdminProducts/>} />
           <Route path="orders" element={<AdminOrders/>} />
           <Route path="features" element={<AdminFeatures/>} />
           <Route path="dashboard" element={<AdminDashboard/>} />
         </Route>
-        <Route path="/shop" element={<ShoppingLayout/>}>
+        <Route path="/shop" element={
+            <CheckAuth isAuthenticated = {isAuthenticated} user = {user}>
+              <ShoppingLayout/>
+            </CheckAuth>
+          }
+        >
           <Route path="account" element={<ShoppingAccount/>}/>
           <Route path="checkout" element={<ShoppingCheckout/>}/>
           <Route path="home" element={<ShoppingHome/>}/>
           <Route path="listing" element={<ShoppingListing/>}/>
 
         </Route>
+          <Route path="/unauth" element={<UnauthPage/>}></Route>
           <Route path="*" element={<NotFound/>}></Route>
       </Routes>
+      
     </div>
   )
 }
---
+++ client/src/indesx.jsx
@@ -0,0 +1,11 @@
+
+
+function UnauthPage() {
+  return(
+    <div>
+      <h1>you don't have access this to page</h1>
+    </div>
+  )
+}
+
+export default UnauthPage;
